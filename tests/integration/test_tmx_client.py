abc61b5a270c383639cd47585fbbd82882aa88c0
Marwin Faiter <noobgubbe@live.se>
2025-03-18 22:57:44 +0100

    updated tests

--- tests/integration/test_tmx_client.py
+++ tests/integration/test_tmx_client.py
@@ -34,7 +34,7 @@ class MockedResponse:
 
 class TestTMXClient(IsolatedAsyncioTestCase):
     def setUp(self):
-        self.base_url = "https://trackmania.exchange/"
+        self.base_url = "https://trackmania.exchange"
         self.tmx_client = TMExchangeClient()
         self.test_map_tags = TestMapTags()
 
@@ -44,7 +44,7 @@ class TestTMXClient(IsolatedAsyncioTestCase):
 
     async def test_get_tags(self):
         expect(ClientSession, times=1).get(
-            f"{self.base_url}api/tags/gettags", **KWARGS,
+            f"{self.base_url}/api/tags/gettags", **KWARGS,
         ).thenReturn(
             MockedResponse(self.test_map_tags.expected_map_tags(), 200),
         )
@@ -52,22 +52,15 @@ class TestTMXClient(IsolatedAsyncioTestCase):
 
     async def test_search_random_map(self):
         expect(ClientSession, times=1).get(
-            f"{self.base_url}api/tags/gettags", **KWARGS,
-        ).thenReturn(
-            MockedResponse(self.test_map_tags.expected_map_tags(), 200),
-        )
-        expect(ClientSession, times=1).get(
-            f"{self.base_url}mapsearch2/search", **KWARGS,
+            f"{self.base_url}/maps", **KWARGS,
         ).thenReturn(
             MockedResponse(self._expected_map_search(), 200),
         )
-        assert await self.tmx_client.search_random_map() == APIMapInfo.from_json(
-            self._expected_map(),
-        )
+        assert await self.tmx_client.search_random_map() == APIMapInfo.from_json(self._expected_map())
 
     async def test_search_mappack(self):
         expect(ClientSession, times=1).get(
-            f"{self.base_url}mappacksearch/search", **KWARGS,
+            f"{self.base_url}/api/mappacks", **KWARGS,
         ).thenReturn(
             MockedResponse(self._expected_map_pack_search(), 200),
         )
@@ -76,102 +69,40 @@ class TestTMXClient(IsolatedAsyncioTestCase):
 
     def _expected_map_search(self):
         return {
-            "results": [self._expected_map()],
+            "Result": [self._expected_map()],
         }
 
     def _expected_map(self):
         return {
-            "TrackID":101290,
-            "UserID":129732,
-            "Username":"Prof_Pinwheel",
-            "GbxMapName":"Toybox Garden",
-            "AuthorLogin":"v9utFEe5Tx6XA_axE2iU3g",
-            "MapType":"TM_Race",
-            "TitlePack":"TMStadium",
-            "TrackUID":"VxVsErO7phpDVTPVfDaYSGb6Vrf",
-            "Mood":"48x48Day",
-            "DisplayCost":2827,
-            "ModName":"",
-            "Lightmap":8,
-            "ExeVersion":"3.3.0",
-            "ExeBuild":"2023-03-31_13_17",
-            "AuthorTime":55008,
-            "ParserVersion":2,
-            "UploadedAt":"2023-04-06T10:33:33.57",
-            "UpdatedAt":"2023-04-06T10:33:33.57",
-            "Name":"Toybox Garden",
-            "Tags":"3,21,39",
-            "TypeName":"Race",
-            "StyleName":"Fragile",
-            "EnvironmentName":"Stadium",
-            "VehicleName":"CarSport",
-            "UnlimiterRequired":False,
-            "RouteName":"Single",
-            "LengthName":"1 min",
-            "DifficultyName":"Advanced",
-            "Laps":1,
-            "ReplayWRID":None,
-            "ReplayWRTime":None,
-            "ReplayWRUserID":None,
-            "ReplayWRUsername":None,
-            "TrackValue":0,
-            "Comments": (
-                "Welcome to Toybox Garden! Remember to play gently with your toys,"
-                " or you might just break them! Happy Hunting!"
-            ),
-            "MappackID":0,
-            "Unlisted":False,
-            "Unreleased":False,
-            "Downloadable":True,
-            "RatingVoteCount":0,
-            "RatingVoteAverage":0.0,
-            "HasScreenshot":False,
-            "HasThumbnail":True,
-            "HasGhostBlocks":True,
-            "EmbeddedObjectsCount":0,
-            "EmbeddedItemsSize":12,
-            "AuthorCount":1,
-            "IsMP4":True,
-            "SizeWarning":False,
-            "AwardCount":0,
-            "CommentCount":0,
-            "ReplayCount":0,
-            "ImageCount":0,
-            "VideoCount":0,
+            "MapId": 229788,
+            "MapUid": "oNSbGPYPbNiAJ37gsjAkL5oqnRb",
+            "UpdatedAt": "2025-03-06T15:55:17.491",
+            "Uploader": {
+                "Name": "koomah"
+            },
+            "Tags": [
+                {
+                    "TagId": 15,
+                    "Name": "Dirt",
+                    "Color": "5e2d09"
+                },
+                {
+                    "TagId": 32,
+                    "Name": "Transitional",
+                    "Color": ""
+                }
+            ],
+            "Name": "go"
         }
 
     def _expected_map_pack_search(self):
         return {
-            "results": [self._expected_map_pack()],
+            "Results": [self._expected_map_pack()],
         }
 
     def _expected_map_pack(self):
         return {
-            "ID":1952,
-            "UserID":26953,
-            "Username":"OregoX",
-            "Name":"Miscellaneous Collection (OregoX)",
-            "Description":"",
-            "TypeName":"Standard",
-            "StyleName":"Mixed",
-            "Titlepack":"Trackmania",
-            "EnvironmentName":"Stadium",
-            "Unreleased":False,
-            "TrackUnreleased":False,
-            "TrackHidden":False,
-            "TrackDownloadable":True,
-            "Downloadable":True,
-            "Downloads":44,
-            "Request":False,
-            "Created":"2022-09-20T07:14:12.043",
-            "Edited":"2022-09-20T18:58:12.463",
-            "VideoURL":"",
-            "TrackCount":24,
-            "MappackValue":0.0,
-            "ShowLB":True,
-            "EndDateLB":None,
-            "TagsString":"1,22,27",
-            "APIOn":False,
-            "AutoApprove":False,
-            "StandardView":None,
+            "MappackId": 1567,
+            "MapCount": 29,
+            "Name": "TOTD - Track of the Day - May 2022"
         }
